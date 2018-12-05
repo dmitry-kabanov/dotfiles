460f3999c6280ef6142cebd1d13800bf701e5a67
Dmitry Kabanov <kabanovdmitry@gmail.com>
2018-12-06 01:06:16 +0300

    [scripts] Refactor script for mounting Shaheen scratch

--- .bin/mount_shaheen_scratch.sh
+++ .bin/mount_shaheen_scratch.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
+#
+# Mount Shaheen scratch folder locally thru SSHFS.
+
+# Mount source and destination paths.
+mount_src="kabanod@shaheen.hpc.kaust.edu.sa:/scratch/kabanod"
+mount_dst="$HOME/shaheen-scratch"
+
 sshfs \
-    kabanod@shaheen.hpc.kaust.edu.sa:/scratch/kabanod \
-    $HOME/shaheen-scratch \
-    -o idmap=user,uid=1000,gid=1000
+    -o idmap=user,uid=1000,gid=1000 \
+    -o ServerAliveInterval=240 \
+    "$mount_src" \
+    "$mount_dst"
