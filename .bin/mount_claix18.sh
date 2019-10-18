12a51137a521d4cfe072735f7380a4ac35f30132
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2019-10-18 17:20:55 +0200

    Add script for mounting CLAIX18 file system

---
+++ .bin/mount_claix18.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Mount my home folder on CLAIX18 locally thru SSHFS.
+
+# Mount source and destination paths.
+username="de217860"
+frontend="copy18-$(($RANDOM % 2 + 1))"
+domain="hpc.itc.rwth-aachen.de"
+hostname="${frontend}.${domain}"
+mount_src="${username}@${hostname}:"
+mount_dst="/mnt/claix18"
+
+echo -n "Mounting hostname ${hostname}..."
+
+sshfs \
+    -o idmap=user,uid=1000,gid=1000 \
+    -o ServerAliveInterval=290 \
+    "$mount_src" \
+    "$mount_dst"
+
+echo " Done"
