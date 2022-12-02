c4cda0b236571bcbf9d50dede4433d333802f014
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2022-12-02 10:59:08 +0100

    [launchd] Compile app from JXA script

--- .launchd/sync-archive/setup_launchd.sh
+++ .launchd/sync-archive/setup_launchd.sh
@@ -1,10 +1,13 @@
 if [[ "$(uname)" == "Darwin" ]]; then
-    name=com.dmitrykabanov.sync-archive.plist
+    name=com.dmitrykabanov.sync-archive
+    plist=${name}.plist
+
+    osacompile -l JavaScript -o  ${name}.app ${name}.js
+
     src=$HOME/.launchd/sync-archive
     target=$HOME/Library/LaunchAgents
-
-    sed -e "s|workdir|${src}|" "${src}/${name}" >"${target}/${name}"
-    launchctl load "${target}/${name}"
+    sed -e "s|workdir|${src}|" "${src}/${plist}" >"${target}/${plist}"
+    launchctl load "${target}/${plist}"
 else
     echo "Not macOS"
 fi
--- .launchd/sync-archive/sync-archive.sh
+++ .launchd/sync-archive/sync-archive.sh
@@ -5,6 +5,8 @@ echo Task "Sync archive" starts at $(date "+%y-%m-%d %H.%M.%S")
 source $HOME/.zshrc-local
 echo "PATH_ARCHIVE_SRC = $PATH_ARCHIVE_SRC"
 echo "PATH_ARCHIVE_DEST = $PATH_ARCHIVE_DEST"
+echo "Sync this: "
+ls -al "$PATH_ARCHIVE_SRC"
 rsync -avz "$PATH_ARCHIVE_SRC" "$PATH_ARCHIVE_DEST"
 
 if [[ $? -eq 0 ]]; then
